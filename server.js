0728a22d2fa6af1dcd38801519f9151d289ab694
ali99944 <ali99944>
2023-09-14 11:24:52 +0300

    update

--- server.js
+++ server.js
@@ -36,18 +36,58 @@ app.use(
     cors({origin: '*'})
 )
 
+const cookieParser = require('cookie-parser')
+app.use(cookieParser())
+
 app.set('view engine', 'ejs')
 app.use(express.static('./public'))
 
-const consultationApi = require('./routes/api/consultation')
+app.get('/login', (req,res) =>{
+    return res.render('auth/login')
+})
+
+app.post('/api/login', async (req,res) =>{
+    const { username, password } = req.body
+
+    console.log(req.body);
+    if(username == "lawyer" && password == "lawyer"){
+        res.cookie('isLogged','true',{
+            maxAge: 36000000000000, // Cookie expiration time in milliseconds (1 hour in this case)
+            httpOnly: true,
+        })
+
+        return res.sendStatus(200)
+    }else{
+        return res.status(500).json({ message: "Error Message"})
+    }
+})
+
+
+app.use((req,res,next) =>{
+    if(req.url.includes('/api/')){
+      return next()
+    }
+
+    if(req.cookies.isLogged == "true" && (req.url != "/login" ) ){
+        next();
+    }else{
+        return res.redirect('/login')
+    }
+})
+
+const consultationApi = require('./routes/api/consulation')
 app.use('/api',consultationApi)
 
-// const userUi = require('./routes/ui/')
-// app.use(userUi)
+const consultationUi = require('./routes/ui/consulations')
+const countryUi = require('./routes/ui/country')
+const lawyerUi = require('./routes/ui/lawyer');
+const { log } = require('console');
+app.use(consultationUi,countryUi,lawyerUi)
 
 app.get('/',(req,res) =>{
     return res.status(200).render('index')
 })
+
 
 app.get('*', (req,res) => {
     return res.status(404).render('handlers/404')
