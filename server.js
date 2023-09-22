f42019425576b830abe47a27bbaddd3a891dc36a
ali99944 <ali99944>
2023-09-22 16:30:18 +0300

    update

--- server.js
+++ server.js
@@ -1,10 +1,31 @@
 require('dotenv').config()
 require('./utils/mongodb_connection')
+const http = require('http');
+const socketIo = require('socket.io');
 
 const express = require('express')
 const app = express()
 const port = process.env.port || 5000
 
+const server = http.createServer(app);
+const io = socketIo(server);
+
+// WebSocket connection handling
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    // Handle messages from the client
+    socket.on('message', (data) => {
+        console.log('Received message:', data);
+        // You can broadcast the message to all connected clients
+        io.emit('message', data);
+    });
+
+    // Handle disconnection
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
 
 const bodyParser = require('body-parser')
 app.use(bodyParser.json())
@@ -60,8 +81,7 @@ app.use('/api',consultationApi)
 const consultationUi = require('./routes/ui/consulations')
 const countryUi = require('./routes/ui/country')
 const lawyerUi = require('./routes/ui/lawyer');
-
-
+const { log } = require('console');
 app.use(consultationUi,countryUi,lawyerUi)
 
 app.get('/',(req,res) =>{
@@ -73,6 +93,8 @@ app.get('*', (req,res) => {
     return res.status(404).render('handlers/404')
 })
 
-app.listen(port, () => {
+// app.listen(port, () => console.log(`running on port ${port}`))
+//
+server.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
 });
